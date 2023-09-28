b02c13007cf0d30c4d6b71574c86110e58756d97
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-09-28 13:32:24 +0530

    added sequelize cli

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const prepareAndStartServer = () => {
     app.listen(3002, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}`); 
     });
 }
 
