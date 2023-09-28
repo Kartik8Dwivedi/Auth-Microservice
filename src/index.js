e473b985f3d47a6589bdfd6e7927daefb083f2a9
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-09-28 16:41:10 +0530

    added encryption using bcrypt and updated registration service

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const apiRoutes = require("./routes/index");
+app.use('/api', apiRoutes);
+
 const prepareAndStartServer = () => {
-    app.listen(3002, () => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`); 
     });
 }
